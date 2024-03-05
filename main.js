7c281c1ab251e814649f7e3f1c582d3577ed65d9
mim chowdhury <umac3333@gmail.com>
2024-03-05 15:04:22 +0600

    lets-discuss-done

--- main.js
+++ main.js
@@ -64,13 +64,19 @@ const loadPost = async () => {
     const data = await res.json();
     const posts = data.posts;
     // console.log(posts);
-    displayPosts(posts)
+    displayPosts(posts);
+    allPost = posts;
+
+
 }
 
 
 const displayPosts = posts => {
     // console.log(posts);
     const postContainer = document.getElementById('allPosts');
+    // postContainer.textContent = '';
+
+
     posts.forEach(post => {
         // console.log(post);
 
@@ -125,22 +131,154 @@ const displayPosts = posts => {
                         ${post.posted_time}
                         min
                     </div>
-                    <button
-                        class="btn bg-[#10B981] mt-4  rounded-full"><i
-                            class="fa-regular fa-envelope text-white"></i></button>
-                </div>
+                    <button onclick="handleMark(${post.id})"
+    class="btn bg-[#10B981] mt-4  rounded-full" > <i
+        class="fa-regular fa-envelope text-white"></i></button >
+                </div >
 
-            </div>
-        </div>
-    </div>
-                    
+            </div >
+        </div >
+    </div >
 
-        
-        
-        `;
+
+
+
+    `;
         postContainer.appendChild(allPosts)
     })
 }
+
+
+
+
+// handle search button
+const loadSearch = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`);
+    const data = await res.json();
+    const posts = data.posts;
+    // console.log(posts);
+    displaySearch(posts)
+
+}
+
+
+const displaySearch = posts => {
+    // console.log(posts);
+    const postContainer = document.getElementById('allPosts');
+    postContainer.textContent = '';
+    posts.forEach(post => {
+        // console.log(post);
+
+        const allPosts = document.createElement('div');
+        allPosts.classList = 'hero bg-base-200 grid grid-cols-1 rounded-3xl ';
+        allPosts.innerHTML = `
+        <div
+        class="hero bg-base-200 grid grid-cols-1 rounded-3xl">
+        <div
+            class="hero-content flex-col lg:flex-row ">
+            <div
+                class="indicator">
+                <span
+                    class="indicator-item badge ${post.isActive ? "bg-[#10B981]" : "bg-[#FF3434]"} "></span>
+                <img class="w-[92px] h-[72px] rounded-2xl lg:mb-28"
+                    src="${post.image}"
+                    class="max-w-sm rounded-lg shadow-2xl" />
+            </div>
+
+
+            <div>
+                <div
+                    class="flex gap-5 mb-2">
+                    <p>#${post.category
+            }
+                    </p>
+                    <p>Author:
+                        ${post.author.name}
+                    </p>
+                </div>
+                <h1
+                    class="text-xl font-bold">
+                    ${post.title}
+                </h1>
+                <p
+                    class="pt-2 pb-2 border-b-2 border-dashed">
+                    ${post.
+                description}
+                </p>
+                <div
+                    class="flex justify-between ">
+                    <div
+                        class="mt-6 space-x-2">
+                        <i
+                            class="fa-regular fa-comment-dots "></i>
+                        ${post.comment_count}
+                        <i
+                            class="fa-regular fa-eye"></i>
+                        ${post.view_count}
+                        <i
+                            class="fa-regular fa-clock"></i>
+                        ${post.posted_time}
+                        min
+                    </div>
+                    <button onclick="handleMark(${post.id})" class="btn bg-[#10B981] mt-4  rounded-full"><i
+    class="fa-regular fa-envelope text-white" ></i ></button >
+                </div >
+
+            </div >
+        </div >
+    </div >
+
+
+
+
+    `;
+        postContainer.appendChild(allPosts)
+    })
+
+}
+
+
+
+const handleSearch = () => {
+    const inputField = document.getElementById('input-field');
+    const searchText = inputField.value;
+    // console.log(searchText);
+    loadSearch(searchText)
+}
+
+
+
+let count = 0
+let allPost = [];
+const loadMark = (id) => {
+    const selectedPost = allPost.find(post => {
+        return post.id.toString() === id;
+    })
+
+
+    displayMark(selectedPost)
+}
+const displayMark = post => {
+    const postContainer = document.getElementById('mark-read');
+
+    const markedPost = document.createElement('div');
+    markedPost.classList = 'flex justify-between gap-2 py-2 ';
+    markedPost.innerHTML = `
+    <h1 class="lg:text-lg lg:font-semibold">${post.title}</h1>
+    <p><i class="fa-regular fa-eye"></i>${post.view_count}</p>
+    `
+
+    postContainer.appendChild(markedPost);
+
+}
+const handleMark = (id) => {
+    count = count + 1;
+    document.getElementById('read-count').innerText = count;
+    loadMark(id.toString())
+
+
+}
+
 
 
 
